3670062fb54435df82aa1110d60987da85d15355
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2024-06-08 10:36:01 +0530

    lombork annotations added

--- Project-1/src/main/java/com/example/Project1/Service/NumberService.java
+++ Project-1/src/main/java/com/example/Project1/Service/NumberService.java
@@ -1,4 +1,13 @@
 package com.example.Project1.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NumberService {
 }
