56d3499b47899516c51e5f59b273f9aa0e75d368
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2024-06-08 10:36:36 +0530

    minor bug fixed

--- Project-1/src/main/java/com/example/Project1/Service/NumberService.java
+++ Project-1/src/main/java/com/example/Project1/Service/NumberService.java
@@ -1,13 +1,7 @@
 package com.example.Project1.Service;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
+
 public class NumberService {
 }
